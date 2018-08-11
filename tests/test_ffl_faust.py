71561f72f5b6abc1bb23f62f19912b9be3903d61
Sean Kell <sean.michael.kell@gmail.com>
2018-08-11 19:41:33 -0400

    test: add example tests from faust docs

--- tests/test_ffl_faust.py
+++ tests/test_ffl_faust.py
@@ -4,22 +4,48 @@
 """Tests for `ffl_faust` package."""
 
 import pytest
+from unittest.mock import Mock, patch
 
-
+import faust
 from ffl_faust import ffl_faust
 
+app = faust.App('example-test-agent-call')
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+@app.agent()
+async def foo(stream):
+    async for value in stream:
+        await bar.send(value)
+        yield value
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+@app.agent()
+async def bar(stream):
+    async for value in stream:
+        yield value + 'YOLO'
 
+@pytest.fixture()
+def test_app():
+    app.finalize()
+    app.conf.store = 'memory://'
+    app.flow_control.resume()
+    return app
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.mark.asyncio()
+async def test_foo(test_app):
+    with patch(__name__ + '.bar') as mocked_bar:
+        mocked_bar.send = mock_coro()
+        async with foo.test_context() as agent:
+            await agent.put('hey')
+            mocked_bar.send.assert_called_with('hey')
+
+def mock_coro(return_value=None, **kwargs):
+    """Create mock coroutine function."""
+    async def wrapped(*args, **kwargs):
+         return return_value
+    return Mock(wraps=wrapped, **kwargs)
+
+@pytest.mark.asyncio()
+async def test_bar(test_app):
+    async with bar.test_context() as agent:
+        event = await agent.put('hey')
+        print(event)
+        assert agent.results[event.message.offset] == 'heyYOLO'
